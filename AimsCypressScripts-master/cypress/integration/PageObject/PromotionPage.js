59cacb64150aa90b4c84cb928d02a39393d578e8
zubair538 <zubairkhanlkl8338@gmail.com>
2022-07-03 23:54:00 +0500

    changes done

---
+++ AimsCypressScripts-master/cypress/integration/PageObject/PromotionPage.js
@@ -0,0 +1,29 @@
+const activity = require('../../fixtures/activity.json')
+const dayjs = require('dayjs')
+
+class ActivityPage{
+   constructor(){}
+   
+
+    EnterPromotionDetail(value){
+        cy.get('#navigation-link-promotions').click();
+      //cy.title().should('eq', 'AIMS-Activity and Incident Management System')
+
+      
+      
+     
+      }
+
+
+
+
+
+
+        return this
+    }
+    
+ 
+    
+
+}
+export default ActivityPage
---
+++ AimsCypressScripts-master/cypress/integration/testCases/testPromotion.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+import LoginPage from '../PageObject/LoginPage';
+const user = require('../../fixtures/user.json')
+
+const PromotionPage = require('../PageObject/PromotionPage');
+
+
+
+
+describe('LoginActivity', function(){
+    const Alogin = new LoginPage();
+    const activity = new PromotionPage();
+   
+    it('valid user name and invalid password', function(){
+        Alogin.visit()
+        Alogin.fillEmail(user.user1);
+        Alogin.fillPassword(user.pass1);
+        Alogin.clickSubmit();
+       // cy.wait(5000)
+     });
+
+     it('Enter the activity Detail', function(){
+         activity.EnterPromotionDetail();
+         //activity.EnterActivtyDetail(false)
+     });
+   
+});
